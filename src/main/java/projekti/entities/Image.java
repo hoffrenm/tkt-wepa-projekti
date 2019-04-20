1a239e180c058dc41abda4c636f9af83b1d7bf14
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-04-21 01:14:07 +0300

    fix

--- src/main/java/projekti/entities/Image.java
+++ src/main/java/projekti/entities/Image.java
@@ -10,12 +10,11 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 /**
@@ -41,5 +40,6 @@ public class Image extends AbstractPersistable<Long> {
     private String contentType;
     private Long contentLength;
     
+    @Type(type = "org.hibernate.type.BinaryType")
     private byte[] content;
 }
