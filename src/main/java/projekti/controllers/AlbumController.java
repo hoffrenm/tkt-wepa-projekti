4421f8adf7bc2c07e397c3d07f2a4929adceb348
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-04-21 00:09:32 +0300

    fix

--- src/main/java/projekti/controllers/AlbumController.java
+++ src/main/java/projekti/controllers/AlbumController.java
@@ -5,22 +5,14 @@
  */
 package projekti.controllers;
 
-import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import projekti.entities.Account;
 import projekti.entities.Image;
 import projekti.repositories.AccountRepository;
